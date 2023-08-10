28d5db6077aed067d6f4dcee85a3724cc8aa7707
JH <ho2755@gmail.com>
2023-08-10 22:12:18 +0900

    2023.08.10  while문 do while 문

---
+++ 인프런C++프로그래밍 입문/섹션 3. Loop/3. while루프와 do while 루프.cpp
@@ -0,0 +1,44 @@
+// 1. while 문
+// 2. do while 문
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	// while문, do while문
+
+	string str = "Panda";
+	int i = 0;
+	while (str[i] != '\0') {
+		cout << str[i] << endl;
+		i++;
+	}
+
+	bool a = true;
+	bool b = false;
+	while (b) {
+		cout << "hello\n";
+	}
+
+	for (int i = 0; i < 3; i++) {	
+		cout << "for문 입니다.\n";
+	}
+
+	int j = 0;
+	while (j < 3) {
+		cout << "while문 입니다\n";
+		j++;
+	}
+
+	cout << i << endl;
+	cout << j << endl;
+
+	bool j1 = false;
+	do {
+		cout << "do while문 입니다.\n";
+	} while (j1);			// do while 문은 반복을 1회 실행하고 조건을 검색해서 true이면 실행
+
+
+	return 0;
+}
